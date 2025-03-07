eb962a5b956d5f55fafc5d07f6a656361c89ac84
vishu <saduvishwanathreddy@gmail.com>
2025-03-07 06:50:50 +0530

    minor style change

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./Components/Header";
 import Template from "./Components/Tamplets";
-import About from "./Components/About";
+// import About from "./Components/About";
 import Footer from "./Components/Footer";
 import Input from "./pages/Input";
 import NotFound from "./pages/NotFound";
@@ -15,7 +15,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/input" element={<Input />} />
           {/* <Route path="/about" element={<About />} /> */}
-          <Route path="/templates" element={<Template/>} />
+          <Route path="/templates" element={<Template />} />
           {/* <Route path="/about" element={<About/>} /> */}
           <Route path="*" element={<NotFound />} />
         </Routes>
